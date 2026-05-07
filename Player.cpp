0e0e53a0405fd494e91e0d50708b4c14970c29b0
azzam <azzamaliabbasi13@gmail.com>
2026-05-08 03:12:05 +0500

    Delete Player.cpp

--- Player.cpp
+++
@@ -1,8 +0,0 @@
-#include "Player.h"
-using namespace std;
-
-Player::Player(const string& n, int id, const string& r)
-    : role(r), name(n), playerID(id), alive(true), voteCount(0)
-{
-    // constructor body is empty - everything is set in the initializer list above
-}
